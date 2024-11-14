ab8b455f96a9406032f8d942a8911aa2ce86e04d
LaFlames <vetalichegga@gmail.com>
2024-11-14 23:40:57 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Home, Main } from "./pages";
+import { Suspense } from "react";
+
 const App = () => {
-  return <div className="app">Hello</div>;
+  return (
+    <div className="app">
+      <Link to="/">Home</Link>
+      <Link to="/main">Main</Link>
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
